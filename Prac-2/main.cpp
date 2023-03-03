4775ebd559133279787640e0985bf88688b6d8d7
jackb-03 <jackbialek2003@gmail.com>
2023-03-03 15:46:50 +1030

    Prac2

---
+++ Prac-2/main.cpp
@@ -0,0 +1,35 @@
+#include "Player.h"
+#include "Referee.h"
+#include "Human.h"
+#include "Computer.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+
+    Computer c;
+    string computerN = c.getName();
+    Human h;
+    string humanN = h.getName();
+    Referee r;
+
+    Player* winner;
+    winner = r.refGame(&h, &c);
+
+    if(winner->getName() == humanN){
+        cout<<humanN<< " Wins";
+    }
+    else if(winner->getName() == computerN){
+        cout<<computerN<< " Wins";
+    }
+    else{
+        cout<<"Draw";
+    }
+
+
+
+    return 0;
+}
